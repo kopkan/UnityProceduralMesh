3f8edab78dff6d4547a48fb81d3cb6ead741dfb4
kopkan <375291152416@yandex.ru>
2021-12-28 02:43:19 +0300

    Update MinMaxValue.cs

--- Shared/MinMaxValue.cs
+++ Shared/MinMaxValue.cs
@@ -1,16 +1,27 @@
 using UnityEngine; //Mathf
 
-namespace Shared
+namespace Wcom.Shared
 {
+    [System.Serializable]
     public class MinMaxValue
     {
-        float _min = float.MaxValue;
-        float _max = -float.MaxValue;
+        [SerializeField]
+        private float _min = float.MaxValue;
+        [SerializeField]
+        private float _max = -float.MaxValue;
 
         public void newContender(float value)
         {
             _min = Mathf.Min(_min, value);
             _max = Mathf.Max(_max, value);
+        }
+
+        public float GGG
+        {
+            get
+            {
+                return _min;
+            }
         }
 
         public float min
